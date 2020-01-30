dd212e247a8767a4e48e9bc4986ee569668b4145
pontakornth <pontakorn61@gmail.com>
2020-01-30 20:36:31 +0700

    Add submission form

---
+++ pages/submit.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from 'next';
+import {
+  Box, Input, Heading, FormControl, FormLabel, Stack, Tag, TagLabel,
+} from '@chakra-ui/core';
+import Background from '../components/Background';
+
+const SubmissionForm: NextPage = () => (
+  <Background>
+    <Box color="white" width={[8 / 10]} mt={1} p={3}>
+      <Heading size="2xl">ส่งชื่ออาหาร</Heading>
+      <FormControl>
+        <FormLabel htmlFor="name">ชื่ออาหาร</FormLabel>
+        <Input placeholder="ต้มยำหมูสับกะเพาะปลา" name="name" />
+        <FormLabel htmlFor="tags">แท็ก</FormLabel>
+        <Stack spacing={4} isInline>
+          <Tag variantColor="green">
+            <TagLabel>ต้มยไำกุ้ง</TagLabel>
+          </Tag>
+        </Stack>
+      </FormControl>
+    </Box>
+  </Background>
+);
