eb80583cc4a9775e08922778c705aa28a2ffbd19
pontakornth <pontakorn61@gmail.com>
2020-01-31 18:56:41 +0700

    Add future Head import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { theme as defaultTheme, CSSReset } from '@chakra-ui/core';
 import { ThemeProvider } from 'emotion-theming';
-const App = ({Component, pageProps}) => (
+// import Head from 'next/head';
+
+const App = ({ Component, pageProps }: any) => (
   <ThemeProvider theme={defaultTheme}>
-      <CSSReset />
-      <Component {...pageProps} />
+    <CSSReset />
+    <Component {...pageProps} />
   </ThemeProvider>
-)
+);
 
 export default App;
