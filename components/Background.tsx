5085c47cb12780fb11957192c4863d5a677b5791
pontakornth <pontakorn61@gmail.com>
2020-01-31 18:56:16 +0700

    Lint

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,6 +1,10 @@
 import { Flex } from '@chakra-ui/core';
 
-const Background : React.FC<{children : any?}> = ({ children }) => (
+interface IBackgroundProps {
+  children: any;
+}
+
+const Background : React.FC<IBackgroundProps> = ({ children }: IBackgroundProps) => (
   <Flex p={4} width="100vw" bg="red.400" height="100vh" justifyContent="center">
     {children}
   </Flex>
