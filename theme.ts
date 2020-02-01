b8e588a808ca60261b2bdb7eafa488c445225626
pontakornth <pontakorn61@gmail.com>
2020-02-01 19:53:46 +0700

    Change font

--- theme.ts
+++ theme.ts
@@ -2,6 +2,11 @@ import { theme as defaultTheme, ITheme as _ITheme } from '@chakra-ui/core';
 
 const theme: _ITheme = {
   ...defaultTheme,
+  fonts: {
+    ...defaultTheme.fonts,
+    heading: "'Mitr', sans-serif",
+    body: "'Itim', sans-serif",
+  },
 
 
 };
